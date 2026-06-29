02a7e1adc28a2ea4ae21ad083994d79da55ba810
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-29 23:27:54 +0530

    added datatypes

---
+++ python-revision/datatypes.py
@@ -0,0 +1,9 @@
+number: int = 10 
+decimal: float = 7.6
+name: str = 'Aditya'
+x: bool = True
+names: list = ['a','b','c']
+y: tuple = (1,2,3)
+z: set = {1,2,3,4,3}
+a: dict = {'name':'bob','age':20}
+print(z)
